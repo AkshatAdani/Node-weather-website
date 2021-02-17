671ffc1c059317367e96a4ab90fb1fe22e21ccf4
Akshat Adani <akshatadani99@gmail.com>
2021-02-17 21:27:48 +0530

    few changes

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,4 +1,4 @@
-const fetchWeather = "/weather";
+
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
@@ -19,23 +19,23 @@ dateElement.textContent = new Date().getDate() + ", " + monthNames[new Date().ge
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    const location = search.value;
     LocationA.textContent = "Loading...";
     Temperature.textContent = "";
     Forecasts.textContent = "";
-    const locationApi = "http://localhost:3000" + fetchWeather + "?address=" + search.value;
-    fetch(locationApi).then(response => {
+    fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
         response.json().then(data => {
             if(data.error) {
                 LocationA.textContent = data.error;
                 Temperature.textContent = "";
                 Forecasts.textContent = "";
             } else {
-                // console.log()
-                // if(data.description === "rain" || data.description === "fog") {
-                //     weatherIcon.className = "wi wi-day-" + data.description
-                // } else {
-                //     weatherIcon.className = "wi wi-day-cloudy"
-                // }
+                console.log()
+                if(data.description === "rain" || data.description === "fog") {
+                    weatherIcon.className = "wi wi-day-" + data.description
+                } else {
+                    weatherIcon.className = "wi wi-day-cloudy"
+                }
                 LocationA.textContent = data.Location;
                 Forecasts.textContent = data.Forecast;
                 Temperature.textContent = data.Temperature + '°C';
